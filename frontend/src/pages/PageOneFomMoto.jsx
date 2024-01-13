15fabce95be6db881e3e97131ef76c068d73b530
sebastien b <bas.envoi2604@gmail.com>
2024-01-13 13:40:01 +0100

    buton retour formulaire

--- frontend/src/pages/PageOneFomMoto.jsx
+++ frontend/src/pages/PageOneFomMoto.jsx
@@ -1,10 +1,18 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import FormMotor from "../components/FormMotor";
 
 function PageOneFomMoto() {
+  const navigate = useNavigate();
+  const handleReturnToApp = () => {
+    navigate("/");
+  };
   return (
     <div>
       <FormMotor />
+      <button type="button" onClick={handleReturnToApp}>
+        Retourner
+      </button>
     </div>
   );
 }
